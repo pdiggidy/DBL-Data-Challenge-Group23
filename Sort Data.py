f606fcd57ed1ae81c12c3a28ee6fc52cd9f6d3e8
yamez <y.ameziane@student.tue.nl>
2022-04-28 12:37:13 +0200

    hcbwe

--- Sort Data.py
+++ Sort Data.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import json
-from tabulate import tabulate
+# from tabulate import tabulate
 
-reader = pd.DataFrame(pd.read_json("airlines-1558527599826.json", lines=True))
+reader = pd.DataFrame(pd.read_json("Data/airlines-1558527599826.json", lines=True))
 #print(tabulate(reader.head(), headers='keys'))
 #for i in range(1,2):
 #    print(reader.iloc[i])
