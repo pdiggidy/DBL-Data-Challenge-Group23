1853236a1f8c6b8b6556ed6b81be2a46e840c568
PNierop <p.c.nierop@student.tue.nl>
2022-04-28 13:30:28 +0200

    Added TestTweets_Repaired.json

--- Sort Data.py
+++ Sort Data.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import json
-from tabulate import tabulate
 
-reader = pd.DataFrame(pd.read_json("airlines-1558527599826.json", lines=True))
-#print(tabulate(reader.head(), headers='keys'))
-#for i in range(1,2):
-#    print(reader.iloc[i])
+# from tabulate import tabulate
+
+reader = pd.DataFrame(pd.read_json("Data/TestTweets_Repaired.json"))
+
 for index in reader.index:
     if reader.iloc[index]['id'] != float(reader.iloc[index]['id_str']):
-            print(reader.iloc[index])
+        print(reader.iloc[index])
