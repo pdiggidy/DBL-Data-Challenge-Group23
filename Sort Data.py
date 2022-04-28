dbd85f01cf525c65e6774f17337d4fa19cc3f655
yamez <y.ameziane@student.tue.nl>
2022-04-28 13:27:59 +0200

    hcbwe

--- Sort Data.py
+++ Sort Data.py
@@ -1,11 +1,25 @@
 import pandas as pd
 import json
-# from tabulate import tabulate
+import os
+from typing import List, Dict
 
-reader = pd.DataFrame(pd.read_json("data/airlines-1558527599826.json", lines=True))
-#print(tabulate(reader.head(), headers='keys'))
-#for i in range(1,2):
-#    print(reader.iloc[i])
+# filenames: List[str] = os.listdir("data")
+# readers: List[pd.DataFrame] = list()
+# print(filenames)
+# for filename in filenames:
+#     dirname = os.path.join("data", filename)
+#     print(dirname)
+#     reader = pd.DataFrame(pd.read_json(dirname, lines=True))
+#     readers.extend(reader)
+
+reader: pd.DataFrame = pd.DataFrame(pd.read_json("data/airlines-1558611772040.json", lines=True, orient="split"))
+
+# reader = readers[0]
 for index in reader.index:
-    if reader.iloc[index]['id'] != float(reader.iloc[index]['id_str']):
-            print(reader.iloc[index])
+    try:
+        if int(reader.iloc[index]['id']) != int(reader.iloc[index]['id_str']):
+                print(reader.iloc[index])
+    except:
+        print(reader.iloc[index])
+# print(reader)
+# print(os.listdir("data"))
