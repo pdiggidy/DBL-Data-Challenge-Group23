a822c767615dd2bd0b98bdb12cc5d1d62d91b539
Philip <p.c.nierop@student.tue.nl>
2022-05-05 22:42:58 +0200

    Beginnings of the function to sort a tweet into companies, so far it only looks at the mentions.

---
+++ CompanySort.py
@@ -0,0 +1,38 @@
+# WORK IN PROGRESS NOT READY TO USE
+
+# KLM    56377143                   0
+# AirFrance   106062176             1
+# British_Airways   18332190        2
+# AmericanAir   22536055            3
+# Lufthansa   124476322             4
+# AirBerlin   26223583              5
+# AirBerlin assist   2182373406     6
+# easyJet     38676903              7
+# RyanAir    1542862735             8
+# SingaporeAir   253340062          9
+# Qantas     218730857              10
+# EtihadAirways   45621423          11
+# VirginAtlantic   20626359         12
+
+company_id_list = [56377143, 106062176, 18332190, 22536055, 124476322, 26223583, 2182373406, 38676903, 1542862735,
+                   253340062, 218730857, 45621423, 20626359]
+
+
+def find_company(company_ids, tweet=None):
+    # This code assumes we've already done the process of removing retweets and switching truncated text
+    mentions = tweet["entities"]["user_mentions"]  # Extract the Mentions Section of the Tweet
+    if len(mentions) != 0:  # if there's no mentions then just skip
+        company = []  # the returned value is a list to account for the possibility that multiple companies mentioned
+        for mention in mentions:
+            user_id = int(mention["id_str"])
+            if user_id in company_ids:
+                # if the mentioned user id is in the list of id's, the index of that id is the number of the company
+                company.append(company_ids.index(user_id))
+        if len(company) != 0:
+            return company
+    text = tweet["text"]  # if no companies are found in the mentions we need to look at the text
+    if text is not None:
+        return 1
+    else:
+        return 0
+
