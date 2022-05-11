666901642496481a1db0dc620b189edee4ea6353
20203707 <y.bouassria@student.tue.nl>
2022-05-11 22:47:02 +0200

    No time for commmit message :(

---
+++ DO NOT TOUCH Plots Back-up.py
@@ -0,0 +1,99 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+import numpy as np
+from main import *
+from DataStatistics import *
+
+# x = [1,2,3,4,5,6,7,8,9]
+# y = [1,1,
+#      2,2,2,2,2,2,
+#      3,3,3,3,3,
+#      4,4,4,
+#      5,
+#      6,6,
+#      7,7,7,
+#      ]
+# y_series = pd.Series(y)
+# print(y_series.mean())
+
+####################################################    PLOT 1
+languages, percentages = tweets_per_language(tweets)
+explode = (0, 0.1, 0, 0)
+fig1, ax1 = plt.subplots()
+ax1.pie(percentages, labels=languages, autopct='%6.1f%%', # shows percentages in pie
+        shadow=False, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+plt.title("Distribution of languages of tweets")
+plt.show()
+
+####################################################    PLOT 2
+tweets_per_day = tweets_per_weekday(tweets)
+categories = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+plt.bar(categories, tweets_per_day, color='maroon',
+        width=0.4)
+plt.xlabel("Label X")
+plt.ylabel("Label Y")
+plt.title("Titel")
+plt.show()
+
+
+####################################################    PLOT 3
+sent, received = tweets_per_airline(tweets)
+x = np.arange(3)
+
+width = 0.40
+plt.bar(x - 0.2, sent, width)
+plt.bar(x + 0.2, received, width)
+plt.show()
+
+####################################################    PLOT 4
+in_business, out_business = tweets_per_hour(tweets)
+fig_4, ax_4 = plt.subplots(ncols=2, nrows=1)
+ax_4_1 = sns.boxplot(data=in_business, ax=ax_4[0])
+#ax_4_1.set_title("Test")
+ax_4_2 = sns.boxplot(data=out_business, ax=ax_4[1])
+fig_4.show()
+
+####################################################    PLOT 5a
+conversation_length_klm,\
+conversation_length_ba,\
+conversation_length_other = average_conversation_length(cleaned_conversations, tweets)
+
+sns.violinplot(data=conversation_length_klm)
+plt.show()
+sns.violinplot(data=conversation_length_ba)
+plt.show()
+sns.violinplot(data=conversation_length_other)
+plt.show()
+
+
+####################################################    PLOT 5b  better?
+conversation_length_klm,\
+conversation_length_ba,\
+conversation_length_other = average_conversation_length(cleaned_conversations, tweets)
+airlines = ['KLM', 'BA', 'Other']
+
+average_lengths = [mean(conversation_length_klm), mean(conversation_length_ba), mean(conversation_length_other)]
+plt.bar(airlines, average_lengths, color='maroon',
+        width=0.4)
+plt.show()
+
+
+####################################################    PLOT 6
+response_klm, response_ba, response_other = average_response_time(tweets)
+fig, ax_6 = plt.subplots(ncols=3, nrows=1, sharey=True)
+ax_1 = sns.boxplot(data=response_klm, ax=ax_6[0])
+ax_2 = sns.boxplot(data=response_ba, ax=ax_6[1])
+ax_3 = sns.boxplot(data=response_other, ax=ax_6[2])
+ax_1.set_ylim([0,400])
+plt.show()
+
+# individual boxplots
+# sns.boxplot(data=response_klm)
+# plt.show()
+# sns.boxplot(data=response_ba)
+# plt.show()
+# sns.boxplot(data=response_other)
+# plt.show()
