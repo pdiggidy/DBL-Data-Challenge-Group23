45f44ec6f7ad3dff51c0d267d5c11fe2bfc7ae6d
PNierop <p.c.nierop@student.tue.nl>
2022-05-24 15:32:07 +0200

    First working sql

--- SQL_test.py
+++ SQL_test.py
@@ -6,7 +6,7 @@ import pymysql
 from sqlalchemy import create_engine
 import os
 
-df_klm = pd.read_pickle(#######)
+df_klm = pd.read_pickle("Clean_data/klm_tweets_df.pickle")
 engine = create_engine(os.environ["DB_STRING"])
 
 df_klm.index = df_klm.index.map(lambda x:int(x))
@@ -15,26 +15,27 @@ df_klm.hashtags = df_klm.hashtags.map(lambda x:f"{x}")
 df_klm.user_mentions = df_klm.user_mentions.map(lambda x:f"{x}")
 
 
-schema_tweets = '''`KLM` (
-`id_str` INT(19) NOT NULL,
-`text` LONGTEXT NOT NULL,
-`in_reply_to_status_id_str` INT,    
-`in_reply_to_user_id_str` INT,
-`place` TEXT,
-`quote_count` INT,
-`reply_count` INT,
-`retweet_count` INT,
-`favorite_count` INT,
-`lang` TEXT,
-`timestamp_ms` BIGINT,
-`hashtags` TEXT,
-`user_mentions` TEXT,
-`user_id_str` TEXT,
-`company` INT,
-`quoted_status_id_str` TEXT,
-`latitude` TEXT,
-`longitude` TEXT,
-UNIQUE KEY `id` (`id_str`) USING HASH,
-PRIMARY KEY (`id_str`)'''
-#df_klm.to_sql('KLM', con=engine, if_exists="append", schema="testing")
-print(len(df_klm))
+# schema_tweets = '''`KLM` (
+# `id_str` INT(19) NOT NULL,
+# `text` LONGTEXT NOT NULL,
+# `in_reply_to_status_id_str` INT,
+# `in_reply_to_user_id_str` INT,
+# `place` TEXT,
+# `quote_count` INT,
+# `reply_count` INT,
+# `retweet_count` INT,
+# `favorite_count` INT,
+# `lang` TEXT,
+# `timestamp_ms` BIGINT,
+# `hashtags` TEXT,
+# `user_mentions` TEXT,
+# `user_id_str` TEXT,
+# `company` INT,
+# `quoted_status_id_str` TEXT,
+# `latitude` TEXT,
+# `longitude` TEXT,
+# UNIQUE KEY `id` (`id_str`) USING HASH,
+# PRIMARY KEY (`id_str`)'''
+
+df_klm.to_sql('BA', con=engine, if_exists="append", schema="Tweets_Data")
+#print(len(df_klm))
