1fd520b7ac08c9f76819a200fed9755e87a2a687
Jeroen Schepers <j.g.h.schepers@student.tue.nl>
2022-06-07 13:35:25 +0200

    a

--- Converdation_sentiment.py
+++ Converdation_sentiment.py
@@ -11,10 +11,10 @@ import numpy as np
 
 connection = create_engine(os.environ["DB_STRING"]).connect()
 df_conv = pd.DataFrame()
-for x in range(1, 5):
+for x in range(1, 50):
     print(x)
     q = f"""
-    SELECT Con.{x} as tweet_id, AT.user_id_str as user_id, {x} as msg_nr, Con.index as conv_id
+    SELECT Con.{x} as tweet_id, AT.user_id_str as user_id, {x} as msg_nr, Con.index as conv_id, AT.sentiment as sentiment
         FROM  Conversations Con
         INNER JOIN All_tweets AT on Con.{x} = AT.id_str
     
@@ -22,6 +22,9 @@ for x in range(1, 5):
 
     df_t = pd.read_sql_query(q, connection)
     df_conv = pd.concat([df_conv, df_t], ignore_index=True)
-df_conv_numpy = df_conv.to_numpy()
+
+df_conv = df_conv.set_index(["conv_id", "msg_nr"])
+df_conv = df_conv.sort_index()
 print(df_conv)
+df_conv_numpy = df_conv.to_numpy()
 connection.close()
