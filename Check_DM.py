5714865e65de4465031c4e741b7815bb50b41998
PNierop <p.c.nierop@student.tue.nl>
2022-06-14 14:44:38 +0200

    regex search

--- Check_DM.py
+++ Check_DM.py
@@ -1,32 +1,34 @@
+import re
+
 from sqlalchemy import create_engine
 import os
 import pandas as pd
 import regex
+import swifter
 
 engine = create_engine(os.environ["DB_STRING"])
 
-# query = """SELECT text
-# FROM All_tweets_labeled, Conversations_max_15_updated
-# WHERE All_tweets_labeled.id_str = Conversations_max_15_updated.tweet_id AND Conversations_max_15_updated.conv_id = Conversations_max_15_updated.conv_id
-# """
 
 query = """SELECT All_tweets.id_str, All_tweets.text, company
     FROM Conversations_max_15_updated Conversations
     INNER JOIN All_tweets_labeled All_tweets on Conversations.tweet_id = All_tweets.id_str
-    WHERE (Conversations.conv_id, Conversations.msg_nr) in 
+    WHERE (Conversations.conv_id, Conversations.msg_nr) in
         (SELECT Conversations.conv_id, max(Conversations.msg_nr)
         FROM Conversations_max_15_updated Conversations group by Conversations.conv_id)
+    AND All_tweets.lang = "en"
 """
 
-#query = """SELECT a.conv_id, tweet_id
-#FROM Conversations_max_15_updated as a
-#INNER JOIN
-#(SELECT conv_id, MAX(msg_nr) as nr
-#FROM Conversations_max_15_updated
-#GROUP BY conv_id) as b
-#ON a.conv_id = b.conv_id AND a.msg_nr = b.nr
-#"""
-df_ends = pd.read_sql(query, engine)
-print()
 
-regex = r'"( DM)"gmi'
+df_ends = pd.read_sql(query, engine)
+
+regex = r'( dm)'
+
+df_ends["check"] = df_ends["text"].swifter.apply(lambda x: re.search(regex, x.lower()))
+
+
+#print("done")
+
+
+#text = "Hello can you send us a DM?"
+
+print(re.search(regex, text.lower()))
