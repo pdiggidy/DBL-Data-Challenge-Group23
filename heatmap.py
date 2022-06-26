b7a7dc896e576ac801656a7e41b347e374c849ad
DATAYoussef <y.135@live.nl>
2022-06-26 21:00:17 +0200

    Updated files

--- heatmap.py
+++ heatmap.py
@@ -28,17 +28,12 @@ AND (C.conv_id, 56377143) IN
 
 df_hm = pd.read_sql_query(query, engine)
 for i in range(len(df_hm)):
-    df_hm.loc[i, "month"] = datetime.fromtimestamp(df_hm.loc[i, "timestamp"] / 1000).strftime('%m')
-    df_hm.loc[i, "day"] = datetime.fromtimestamp(df_hm.loc[i, "timestamp"] / 1000).strftime('%d')
+    df_hm.loc[i, "Month"] = datetime.fromtimestamp(df_hm.loc[i, "timestamp"] / 1000).strftime('%m')
+    df_hm.loc[i, "Day"] = datetime.fromtimestamp(df_hm.loc[i, "timestamp"] / 1000).strftime('%d')
 
+df_hm.loc[len(df_hm)] = [0, 0, 0, 0, 0, 0, 0, 0, "04", "01"]  # This is so April is in the heatmap
 df_hm = df_hm.set_index(["conv_id", "inter_nr", "msg_nr"]).sort_index()
-df_hm.to_sql("HeatmapData", engine, schema="Tweets_Data", if_exists="replace")
-
-
-df_hm = pd.read_sql_table("HeatmapData", engine)
-df_hm.loc[len(df_hm)] = [0, 0, 0, 0, 0, 0, 0, 0, "04", "01"]
-
-
+# df_hm.to_sql("HeatmapData", engine, schema="Tweets_Data", if_exists="replace")
 
 hmdata = df_hm.groupby(["Month", "Day"])["sentiment_change"].mean().dropna().to_frame(name='total_sentiment').reset_index()
 hmdata = hmdata.reset_index().pivot(columns='Month', index='Day', values='total_sentiment')
